1ddb240439734a1ebd101e3c691a420877a067e8
zhangjun <zhangjun.julian@gmail.com>
2014-10-31 16:47:13 +0800

    change code to step by step mode

--- loadData.hs
+++ loadData.hs
@@ -3,15 +3,32 @@ import Data.Ord
 import Text.CSV.Lazy.String
 import Data.List
 
-                
+splitLength = 50
+
+mySplit' [] = []
+mySplit' xs = [x] ++  mySplit' t 
+	where
+	x = take splitLength xs 
+	t = drop splitLength xs                
+
+--mySplit::[a]->[(int,[a])]
+mySplit xs = zip [1..] ( mySplit' xs)
+
+getCountData::Ord a => [[a]] -> [[(a,Int)]]
+getCountData t =  map ( reverse . sortBy (comparing snd)) $ map 
+	(map (\x -> ((head x),length x))) $ 
+	map group $ map sort $ transpose t
 
 loadTestData = do
 	testFile <- readFile "data/test_rev2"
 	let cfile = fromCSVTable $ csvTable $ parseCSV testFile
 	let coloum = head cfile
-	let body = tail cfile
-	let coloumList = map ( reverse . sortBy (comparing snd)) $ map (map (\x -> ((head x),length x))) $  map group $ map sort $ transpose body 
-	let output = zip coloum coloumList
-	writeFile "testdata"  $ unlines $ (map show output)
+	let body = take 1000 $ tail cfile
+	let countData = map (\(x,y) -> (x , getCountData y )) $ mySplit body 
+	-- let output = zip coloum coloumList
+	--writeFile "testdata"  $ unlines $ (map show output)
+	return countData
+
+
 
 --sortBy (comparing  snd) $ map (\x -> ((head x),length x)) $ 
