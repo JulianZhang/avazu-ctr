8f8956292d28f5cc6554675ddcba95edb852b53c
zhangjun <zhangjun.julian@gmail.com>
2014-10-31 13:48:57 +0800

    use lazy-csv for performance

--- loadData.hs
+++ loadData.hs
@@ -1,19 +1,8 @@
-import Text.Parsec (parse,anyChar)
-import Text.Parsec.String --(Parser)
---import Text.Parsec.String.Parsec --(try)
-import Text.Parsec.Char (oneOf, char, digit, satisfy,letter,spaces)
-import Text.Parsec.Combinator (many1, choice, chainl1,option,eof,sepBy)
-import Control.Applicative ((<|>), many,(<*) ,(*>),(<*>))
-import Control.Monad --(void)
-import Data.Char --(isLetter, isDigit)
+
+import Text.CSV.Lazy.String
 
 
-data SimpleExpr = Num Integer
-                 | Var String
-                 | Add SimpleExpr SimpleExpr
-                 | Parens SimpleExpr
-                 | Str [Char]
-                   deriving (Eq,Show)
+
 
 usedChar = choice [letter,digit,(char '_'),(char '-')]
 
@@ -21,7 +10,9 @@ csvSep =  sepBy (many1 usedChar) (char ',')
 
 loadTestData = do
 	testFile <- readFile "data/test_rev2"
-	let coloum = parse  csvSep "fail" $ head $ lines testFile 
-	return coloum
+	let cfile = parseCSV testFile
+	let coloum = head cfile
+	let body = tail cfile 
+	return $ head $ fromCSVTable $ csvTable  body
 
 test = parse csvSep "fail" "sfsdf,sdfsdf,sdfsdf,12312,234d"
