8b5e0b7bc1a9e0594a8a458816e2aba8dd63650a
zhangjun <zhangjun.julian@gmail.com>
2014-10-31 16:06:28 +0800

    can't work with 4m records

--- loadData.hs
+++ loadData.hs
@@ -9,9 +9,9 @@ loadTestData = do
 	testFile <- readFile "data/test_rev2"
 	let cfile = fromCSVTable $ csvTable $ parseCSV testFile
 	let coloum = head cfile
-	let body = take 1000 $ tail cfile
+	let body = tail cfile
 	let coloumList = map ( reverse . sortBy (comparing snd)) $ map (map (\x -> ((head x),length x))) $  map group $ map sort $ transpose body 
 	let output = zip coloum coloumList
-	return   output
+	writeFile "testdata"  $ unlines $ (map show output)
 
 --sortBy (comparing  snd) $ map (\x -> ((head x),length x)) $ 
