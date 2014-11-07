16afb18b5457e2199401cd84116361133583baf2
zhangjun <zhangjun.julian@gmail.com>
2014-11-07 17:05:53 +0800

    change to handle list mode, but read is slow

--- fileStore.hs
+++ fileStore.hs
@@ -10,19 +10,30 @@ import qualified Data.Map as DM
 import Control.Monad
 import Data.List
 
+
+
 workdir = "workdata/"
 
-batchSize = 20000
+batchSize = 2000
 
-readFromPipes::[String]->Consumer [String] IO ()
+readFromPipes::[Handle]->Consumer [String] IO ()
 readFromPipes head = do
 	body <- await
-	lift $ append2FileLIst workdir head body
+	lift $ append2HandleLIst  head body
 	readFromPipes head
 
 
+append2HandleLIst handleList dataList = do 
+	zipWithM_ (\x y-> hPutStr x y) handleList dataList
+
 append2FileLIst dir fNameList dataList = do
 	zipWithM_ (\x y -> appendFile (dir++x ::FilePath) y) fNameList dataList
+
+openFiles fNameList = do
+	mapM (\x -> openFile  (workdir++x::FilePath) AppendMode ) fNameList
+
+closeFiles handleList = do
+	mapM_ hClose handleList
 
 main = do 
 	s<- getArgs
@@ -30,8 +41,10 @@ main = do
 	let readCsv = splitCSV 
 	withFile "data/train_rev2" ReadMode $ \h -> do 
 		csvHead <- hGetLine h 
+		handleList <- openFiles (readCsv csvHead)
 		runEffect $ 
 			readFileBatch h batchSize readCsv  ( (map unlines) . transpose) 
-			>-> P.take num >-> readFromPipes (readCsv csvHead)
+			>-> P.take num >-> readFromPipes handleList
+		closeFiles handleList
 --		let outList = zip  (readCsv csvHead)  $ map DM.toList t 
 --		mapM (\x -> appendFile "testdata"  ((show x)++ "\n" ) ) outList
