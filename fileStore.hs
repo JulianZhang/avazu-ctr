7f3fb9e0eab2c763b431b42dd6fc8562f41aad64
zhangjun <zhangjun.julian@gmail.com>
2014-11-07 16:03:55 +0800

    work, but less than 3M/S work load

--- fileStore.hs
+++ fileStore.hs
@@ -8,22 +8,30 @@ import qualified Pipes.Prelude as P
 
 import qualified Data.Map as DM
 import Control.Monad
+import Data.List
 
+workdir = "workdata/"
+
+batchSize = 20000
+
+readFromPipes::[String]->Consumer [String] IO ()
+readFromPipes head = do
+	body <- await
+	lift $ append2FileLIst workdir head body
+	readFromPipes head
 
 
 append2FileLIst dir fNameList dataList = do
-
 	zipWithM_ (\x y -> appendFile (dir++x ::FilePath) y) fNameList dataList
 
-main = do
- return "" 
---	s<- getArgs
---	let num =  (1+) $ (\x -> div x splitLength)  $ (read . head) s 
---	let readCsv = splitCSV 
---	withFile "data/test_rev2" ReadMode $ \h -> do 
---		csvHead <- hGetLine h 
---		t <- runEffect $ 
---			readFileBatch h 50 readCsv  transpose 
---			>-> P.take num
+main = do 
+	s<- getArgs
+	let num =  (1+) $ (\x -> div x batchSize)  $ (read . head) s 
+	let readCsv = splitCSV 
+	withFile "data/train_rev2" ReadMode $ \h -> do 
+		csvHead <- hGetLine h 
+		runEffect $ 
+			readFileBatch h batchSize readCsv  ( (map unlines) . transpose) 
+			>-> P.take num >-> readFromPipes (readCsv csvHead)
 --		let outList = zip  (readCsv csvHead)  $ map DM.toList t 
 --		mapM (\x -> appendFile "testdata"  ((show x)++ "\n" ) ) outList
